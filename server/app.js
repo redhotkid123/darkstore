a865054d9f32be0f830057495b0c248a434fc38b
Jihyun Min <jihyunjomin@gmail.com>
2016-05-05 12:41:46 -0700

    postgres

--- server/app.js
+++ server/app.js
@@ -15,6 +15,19 @@ var swig = require('swig');
 // *** express instance *** //
 var app = express();
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 // *** view engine *** //
 var swig = new swig.Swig();
