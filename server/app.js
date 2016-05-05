85b7bb2fb646a48ef354ffc20bb5107ec1f290d1
Jihyun Min <jihyunjomin@gmail.com>
2016-05-05 12:56:06 -0700

    postgres

--- server/app.js
+++ server/app.js
@@ -16,17 +16,12 @@ var swig = require('swig');
 var app = express();
 
 var pg = require('pg');
+var db = require('./models');
 
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
+db.sequelize.sync().then(function() {
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
 });
 
 // *** view engine *** //
--- server/models/index.js
+++ server/models/index.js
@@ -8,7 +8,15 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
+if(process.env.HEROKU_POSTGRESQL_BRONZE_URL) {
+  var sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    port: match[4],
+    host: match[3],
+    logging: true
+  })
+} else if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
